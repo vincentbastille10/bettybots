99eb5a9ea3e6dfa20b45feb58aa4446b255bbc65
vincentbastille10 <135065221+vincentbastille10@users.noreply.github.com>
2025-10-20 11:21:23 +0200

    Create __init__.py

---
+++ betty_rules/__init__.py
@@ -0,0 +1,6 @@
+
+from .loader import load_pack
+from .memory import Memory
+from .nlu_rules import match_intent
+from .dialog_manager import DialogManager
+from .templates_engine import render_template
